6c718465437dc402e59620874ccd44f5ad6932d6
Barbariansyah <bariansyahi@gmail.com>
2020-09-05 22:41:54 +0700

    use hooks

--- components/TextForm/index.tsx
+++ components/TextForm/index.tsx
@@ -1,21 +1,38 @@
-import React, { FunctionComponent } from 'react';
+import React, { useState, FunctionComponent } from 'react';
 import { Row, Button } from 'react-bootstrap';
+import { useInput } from '../../util/useInput';
 
 const TextForm: FunctionComponent = () => {
+    const { value: plainText, bind: bindPlainText, setValue: setPlainText } = useInput('');
+    const { value: decryptedText, bind: bindDecryptedText, setValue: setDecryptedText } = useInput('');
+    const { value: algorithm, bind: bindAlgorithm } = useInput('Vignere Cipher');
+    const handleEncrypt = () => {
+        setDecryptedText(plainText + ' - ' + algorithm);
+    };
+    const handleDecrypt = () => {
+        setPlainText(decryptedText + ' - ' + algorithm);
+    };
     return (
         <>
-            Enter your text
-            <textarea name="message" rows={5} cols={70} />
-            <select>
-                <option>Vignere Cipher</option>
-                <option>Full Vignere Cipher</option>
-                <option>Auto Key Vignere Cipher</option>
-            </select>
-            <Row className="mt-2 justify-content-center">
-                <Button variant="primary">Encrypt</Button>
+            Plain text
+            <textarea {...bindPlainText} name="message" rows={5} cols={70} />
+            <Row className="mt-2 mb-2 justify-content-around">
+                <select {...bindAlgorithm}>
+                    <option value="1">Vignere Cipher</option>
+                    <option value="2">Full Vignere Cipher</option>
+                    <option value="3">Auto Key Vignere Cipher</option>
+                </select>
+                <Button variant="primary" onClick={handleEncrypt}>
+                    Encrypt
+                </Button>
+                <Button variant="primary" onClick={handleDecrypt}>
+                    Decrypt
+                </Button>
             </Row>
             Encrypted text
-            <textarea name="encrypted_message" rows={5} cols={70} />
+            <textarea {...bindDecryptedText} name="encrypted_message" rows={5} cols={70}>
+                {decryptedText}
+            </textarea>
         </>
     );
 };
